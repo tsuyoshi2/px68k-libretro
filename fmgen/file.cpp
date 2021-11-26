f7cc532a0b29f26ce3b942d74b4feba6fa01882c
twinaphex <libretro@gmail.com>
2021-11-26 18:03:05 +0100

    (FMGEN) Slim down file I/O class

--- fmgen/file.cpp
+++ fmgen/file.cpp
@@ -33,8 +33,6 @@ bool FileIO::Open(const char* filename, uint32_t flg)
 {
 	Close();
 
-	strncpy(path, filename, MAX_PATH);
-
 	DWORD access = (flg & readonly ? 0 : GENERIC_WRITE) | GENERIC_READ;
 	DWORD share = (flg & readonly) ? FILE_SHARE_READ : 0;
 	DWORD creation = flg & create ? CREATE_ALWAYS : OPEN_EXISTING;
@@ -42,39 +40,6 @@ bool FileIO::Open(const char* filename, uint32_t flg)
 	hfile = CreateFile(filename, access, share, 0, creation, 0, 0);
 	
 	flags = (flg & readonly) | (hfile == INVALID_HANDLE_VALUE ? 0 : open);
-	if (!(flags & open))
-	{
-		switch (FAKE_GetLastError())
-		{
-		case ERROR_FILE_NOT_FOUND:		error = file_not_found; break;
-		case ERROR_SHARING_VIOLATION:	error = sharing_violation; break;
-		default: error = unknown; break;
-		}
-	}
-	SetLogicalOrigin(0);
-
-	return !!(flags & open);
-}
-
-// ---------------------------------------------------------------------------
-//	ファイルがない場合は作成
-// ---------------------------------------------------------------------------
-
-bool FileIO::CreateNew(const char* filename)
-{
-	Close();
-
-	strncpy(path, filename, MAX_PATH);
-
-	DWORD access = GENERIC_WRITE | GENERIC_READ;
-	DWORD share = 0;
-	DWORD creation = CREATE_NEW;
-
-	hfile = CreateFile(filename, access, share, 0, creation, 0, 0);
-	
-	flags = (hfile == INVALID_HANDLE_VALUE ? 0 : open);
-	SetLogicalOrigin(0);
-
 	return !!(flags & open);
 }
 
@@ -84,7 +49,8 @@ bool FileIO::CreateNew(const char* filename)
 
 void FileIO::Close()
 {
-	if (GetFlags() & open)
+	uint32_t flags = GetFlags();
+	if (flags & open)
 	{
 		FAKE_CloseHandle(hfile);
 		flags = 0;
@@ -97,7 +63,8 @@ void FileIO::Close()
 
 int32_t FileIO::Read(void* dest, int32_t size)
 {
-	if (!(GetFlags() & open))
+	uint32_t flags = GetFlags();
+	if (!(flags & open))
 		return -1;
 	
 	DWORD readsize;
@@ -110,31 +77,21 @@ int32_t FileIO::Read(void* dest, int32_t size)
 //	ファイルへの書き出し
 // ---------------------------------------------------------------------------
 
-int32_t FileIO::Write(const void* dest, int32_t size)
-{
-	if (!(GetFlags() & open) || (GetFlags() & readonly))
-		return -1;
-	
-	DWORD writtensize;
-	if (!WriteFile(hfile, dest, size, &writtensize, 0))
-		return -1;
-	return writtensize;
-}
-
 // ---------------------------------------------------------------------------
 //	ファイルをシーク
 // ---------------------------------------------------------------------------
 
 bool FileIO::Seek(int32_t pos, SeekMethod method)
 {
-	if (!(GetFlags() & open))
+        uint32_t flags = GetFlags();
+	if (!(flags & open))
 		return false;
 	
 	DWORD wmethod;
 	switch (method)
 	{
 	case begin:	
-		wmethod = FILE_BEGIN; pos += lorigin; 
+		wmethod = FILE_BEGIN; 
 		break;
 	case current:	
 		wmethod = FILE_CURRENT; 
--- fmgen/file.h
+++ fmgen/file.h
@@ -36,23 +36,16 @@ public:
 	virtual ~FileIO();
 
 	bool Open(const char* filename, uint32_t flg = 0);
-	bool CreateNew(const char* filename);
 	void Close();
-	Error GetError() { return error; }
 
 	int32_t Read(void* dest, int32_t len);
-	int32_t Write(const void* src, int32_t len);
 	bool Seek(int32_t fpos, SeekMethod method);
 
 	uint32_t GetFlags() { return flags; }
-	void SetLogicalOrigin(int32_t origin) { lorigin = origin; }
 
 private:
 	HANDLE hfile;
 	uint32_t flags;
-	uint32_t lorigin;
-	Error error;
-	char path[MAX_PATH];
 	
 	FileIO(const FileIO&);
 	const FileIO& operator=(const FileIO&);
