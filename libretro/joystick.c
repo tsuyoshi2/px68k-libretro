7343484739a295a24060d5bb3b2e2d8cab440765
libretroadmin <reallibretroretroarch@gmail.com>
2023-01-14 19:06:32 +0100

    (Joystick) Cleanups

--- libretro/joystick.c
+++ libretro/joystick.c
@@ -12,35 +12,33 @@ extern uint32_t libretro_supports_input_bitmasks;
 
 uint8_t joy[2];
 uint8_t JoyKeyState;
-uint8_t JoyKeyState0;
-uint8_t JoyKeyState1;
-uint8_t JoyState0[2];
-uint8_t JoyState1[2];
+static uint8_t JoyKeyState0;
+static uint8_t JoyKeyState1;
+
+static uint8_t JoyState0[2];
+static uint8_t JoyState1[2];
+static uint8_t JoyPortData[2];
 
 /* This stores whether the buttons were down. This avoids key repeats. */
-uint8_t JoyDownState0;
-uint8_t MouseDownState0;
-
-uint8_t JoyPortData[2];
+static uint8_t JoyDownState0;
 
 void Joystick_Init(void)
 {
-	joy[0] = 1; /* activate JOY1 */
-	joy[1] = 1; /* activate JOY2 */
-	JoyKeyState = 0;
-	JoyKeyState0 = 0;
-	JoyKeyState1 = 0;
-	JoyState0[0] = 0xff;
-	JoyState0[1] = 0xff;
-	JoyState1[0] = 0xff;
-	JoyState1[1] = 0xff;
+	joy[0]         = 1; /* activate JOY1 */
+	joy[1]         = 1; /* activate JOY2 */
+	JoyKeyState    = 0;
+	JoyKeyState0   = 0;
+	JoyKeyState1   = 0;
+	JoyState0[0]   = 0xff;
+	JoyState0[1]   = 0xff;
+	JoyState1[0]   = 0xff;
+	JoyState1[1]   = 0xff;
 	JoyPortData[0] = 0;
 	JoyPortData[1] = 0;
 }
 
 void Joystick_Cleanup(void)
 {
-
 }
 
 uint8_t FASTCALL Joystick_Read(uint8_t num)
@@ -53,13 +51,14 @@ uint8_t FASTCALL Joystick_Read(uint8_t num)
 		ret1 = JoyState1[num];
 	}
 
-	return ((~JoyPortData[num])&ret0)|(JoyPortData[num]&ret1);
+	return ((~JoyPortData[num]) & ret0) | (JoyPortData[num] & ret1);
 }
 
 
 void FASTCALL Joystick_Write(uint8_t num, uint8_t data)
 {
-	if ( (num==0)||(num==1) ) JoyPortData[num] = data;
+	if ((num == 0)||(num == 1))
+      JoyPortData[num] = data;
 }
 
 /* Menu navigation related vars */
@@ -87,22 +86,26 @@ static uint16_t get_px68k_input(int port)
 
 void FASTCALL Joystick_Update(int is_menu, int key, int port)
 {
-	uint8_t ret0 = 0xff, ret1 = 0xff;
-	uint8_t mret0 = 0xff, mret1 = 0xff;
-	uint8_t temp = 0;
+	uint8_t ret0            = 0xff, ret1 = 0xff;
+	uint8_t mret0           = 0xff, mret1 = 0xff;
+	uint8_t temp            = 0;
 	static uint8_t pre_ret0 = 0xff, pre_mret0 = 0xff;
-	uint32_t res = 0;
+	uint32_t res            = 0;
 
 	if (libretro_supports_input_bitmasks)
-		res = get_px68k_input_bitmasks(port);
+		res                  = get_px68k_input_bitmasks(port);
 	else
-		res = get_px68k_input(port);
+		res                  = get_px68k_input(port);
 
 	/* D-Pad */
-	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_RIGHT))	temp |= JOY_RIGHT;
-	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_LEFT))	temp |=  JOY_LEFT;
-	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_UP))		temp |=  JOY_UP;
-	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_DOWN))	temp |=  JOY_DOWN;
+	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_RIGHT))
+      temp |= JOY_RIGHT;
+	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_LEFT))
+      temp |=  JOY_LEFT;
+	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_UP))
+      temp |=  JOY_UP;
+	if (res & (1 << RETRO_DEVICE_ID_JOYPAD_DOWN))
+      temp |=  JOY_DOWN;
 
 	if ((temp & (JOY_LEFT | JOY_RIGHT)) == (JOY_LEFT | JOY_RIGHT))
 		temp &= ~(JOY_LEFT | JOY_RIGHT);
@@ -112,56 +115,69 @@ void FASTCALL Joystick_Update(int is_menu, int key, int port)
 	ret0 ^= temp;
 
 	/* Buttons */
-	switch (Config.JOY_TYPE[port]) {
-	case PAD_2BUTTON:
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_A))	ret0 ^= (Config.VbtnSwap ? JOY_TRG1 : JOY_TRG2);
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_B))	ret0 ^= (Config.VbtnSwap ? JOY_TRG2 : JOY_TRG1);
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_X))	ret0 ^= (Config.VbtnSwap ? JOY_TRG2 : JOY_TRG1);
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_Y))	ret0 ^= (Config.VbtnSwap ? JOY_TRG1 : JOY_TRG2);
+	switch (Config.JOY_TYPE[port])
+   {
+      case PAD_2BUTTON:
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_A))
+            ret0 ^= (Config.VbtnSwap ? JOY_TRG1 : JOY_TRG2);
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_B))
+            ret0 ^= (Config.VbtnSwap ? JOY_TRG2 : JOY_TRG1);
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_X))
+            ret0 ^= (Config.VbtnSwap ? JOY_TRG2 : JOY_TRG1);
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_Y))
+            ret0 ^= (Config.VbtnSwap ? JOY_TRG1 : JOY_TRG2);
 
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_START)) ret0 &= ~(JOY_UP | JOY_DOWN);
-		if (!Config.joy1_select_mapping)
-			if (res & (1 << RETRO_DEVICE_ID_JOYPAD_SELECT)) ret0 &= ~(JOY_LEFT | JOY_RIGHT);
-		break;
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_START))
+            ret0 &= ~(JOY_UP | JOY_DOWN);
+         if (!Config.joy1_select_mapping)
+            if (res & (1 << RETRO_DEVICE_ID_JOYPAD_SELECT))
+               ret0 &= ~(JOY_LEFT | JOY_RIGHT);
+         break;
 
-	case PAD_CPSF_MD:
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_A))	ret0 ^= JOY_TRG1;	/* Low-Kick */
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_B))	ret0 ^= JOY_TRG2;	/* Mid-Kick */
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_X))	ret1 ^= JOY_TRG4; 	/* Low-Punch */
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_Y))	ret1 ^= JOY_TRG3;	/* Mid-Punch */
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_L))	ret1 ^= JOY_TRG5;	/* High-Punch */
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_R))	ret1 ^= JOY_TRG8;	/* High-Kick */
+      case PAD_CPSF_MD:
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_A))
+            ret0 ^= JOY_TRG1;	/* Low-Kick */
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_B))
+            ret0 ^= JOY_TRG2;	/* Mid-Kick */
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_X))
+            ret1 ^= JOY_TRG4; 	/* Low-Punch */
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_Y))
+            ret1 ^= JOY_TRG3;	/* Mid-Punch */
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_L))
+            ret1 ^= JOY_TRG5;	/* High-Punch */
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_R))
+            ret1 ^= JOY_TRG8;	/* High-Kick */
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_START))
+            ret1 ^= JOY_TRG6; /* Start */
+         if (!Config.joy1_select_mapping)
+            if (res & (1 << RETRO_DEVICE_ID_JOYPAD_SELECT))
+               ret1 ^= JOY_TRG7;	/* Mode */
+         break;
 
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_START))	ret1 ^= JOY_TRG6; /* Start */
-		if (!Config.joy1_select_mapping)
-			if (res & (1 << RETRO_DEVICE_ID_JOYPAD_SELECT))	ret1 ^= JOY_TRG7;	/* Mode */
-		break;
+      case PAD_CPSF_SFC:
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_A))
+            ret0 ^= JOY_TRG2;
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_B))
+            ret0 ^= JOY_TRG1;
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_X))
+            ret1 ^= JOY_TRG3;
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_Y))
+            ret1 ^= JOY_TRG4;
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_L))
+            ret1 ^= JOY_TRG8;
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_R))
+            ret1 ^= JOY_TRG5;
+         if (res & (1 << RETRO_DEVICE_ID_JOYPAD_START))
+            ret1 ^= JOY_TRG6;
+         if (!Config.joy1_select_mapping)
+            if (res & (1 << RETRO_DEVICE_ID_JOYPAD_SELECT))
+               ret1 ^= JOY_TRG7;
+         break;
+   }
 
-	case PAD_CPSF_SFC:
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_A))	ret0 ^= JOY_TRG2;
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_B))	ret0 ^= JOY_TRG1;
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_X))	ret1 ^= JOY_TRG3;
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_Y))	ret1 ^= JOY_TRG4;
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_L))	ret1 ^= JOY_TRG8;
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_R))	ret1 ^= JOY_TRG5;
-
-		if (res & (1 << RETRO_DEVICE_ID_JOYPAD_START))	ret1 ^= JOY_TRG6;
-		if (!Config.joy1_select_mapping)
-			if (res & (1 << RETRO_DEVICE_ID_JOYPAD_SELECT))	ret1 ^= JOY_TRG7;
-		break;
-	}
-
-	JoyDownState0 = ~(ret0 ^ pre_ret0) | ret0;
-	pre_ret0 = ret0;
-
-	MouseDownState0 = ~(mret0 ^ pre_mret0) | mret0;
-	pre_mret0 = mret0;
-
-	/* disable Joystick when software keyboard is active */
-	if (!is_menu && !Keyboard_IsSwKeyboard()) {
-		JoyState0[port] = ret0;
-		JoyState1[port] = ret1;
-	}
+	JoyDownState0   = ~(ret0 ^ pre_ret0) | ret0;
+	pre_ret0        = ret0;
+	pre_mret0       = mret0;
 
 	/* input overrides section during Menu mode for faster menu browsing
 	 * by pressing and holding key or button aka turbo mode */
@@ -189,30 +205,39 @@ void FASTCALL Joystick_Update(int is_menu, int key, int port)
 			repeat_delay  = JOYDELAY;
 			repeat_rate   = 0;
 			JoyDownState0 = (inbuf ^ 0xff);
-		} else {
-			if (repeat_delay)
-				repeat_delay--;
-			if (repeat_delay == 0) {
-				if (repeat_rate)
-					repeat_rate--;
-				if (repeat_rate == 0) {
-					repeat_rate = RATE;
-					for (i = 0; i < 4; i++) {
-						uint8_t tmp = (1 << i); /* which direction? UP/DOWN/LEFT/RIGHT */
-						if ((inbuf & tmp) == tmp)
-							speedup_joy[tmp] = 1;
-					}
-				}
-			}
 		}
+      else
+      {
+         if (repeat_delay)
+            repeat_delay--;
+         if (repeat_delay == 0)
+         {
+            if (repeat_rate)
+               repeat_rate--;
+            if (repeat_rate == 0)
+            {
+               repeat_rate = RATE;
+               for (i = 0; i < 4; i++)
+               {
+                  uint8_t tmp = (1 << i); /* which direction? UP/DOWN/LEFT/RIGHT */
+                  if ((inbuf & tmp) == tmp)
+                     speedup_joy[tmp] = 1;
+               }
+            }
+         }
+      }
 	}
+   else
+   {
+      /* disable Joystick when software keyboard is active */
+      if (!Keyboard_IsSwKeyboard())
+      {
+         JoyState0[port] = ret0;
+         JoyState1[port] = ret1;
+      }
+
+   }
 }
 
-uint8_t get_joy_downstate(void)
-{
-	return JoyDownState0;
-}
-void reset_joy_downstate(void)
-{
-	JoyDownState0 = 0xff;
-}
+uint8_t get_joy_downstate(void)   { return JoyDownState0;  }
+void    reset_joy_downstate(void) { JoyDownState0 = 0xff;  }
